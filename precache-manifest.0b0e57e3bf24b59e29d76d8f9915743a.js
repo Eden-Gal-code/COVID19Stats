8dc6544d7bafc9c6a2da7e1077a21ebd10cda519
Eden Gal <eden.gal230897@gmail.com>
2020-04-12 17:53:45 +0300

    Updates

---
+++ precache-manifest.0b0e57e3bf24b59e29d76d8f9915743a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55efdc9e0100c9b975a277a1e43945cc",
+    "url": "/COVID19Stats/index.html"
+  },
+  {
+    "revision": "fff96b090e06ed605240",
+    "url": "/COVID19Stats/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "71ad21ed0fdb46d86a7a",
+    "url": "/COVID19Stats/static/js/2.0b85abb8.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/COVID19Stats/static/js/2.0b85abb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff96b090e06ed605240",
+    "url": "/COVID19Stats/static/js/main.655c98ee.chunk.js"
+  },
+  {
+    "revision": "6c3362b8e5f6a792b1b6",
+    "url": "/COVID19Stats/static/js/runtime-main.22bcc0d9.js"
+  }
+]);
